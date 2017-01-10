3283fd055e8a465d90788e541076985eb97d3897
joshkmcdonald <joshkmcdonald@gmail.com>
2017-01-10 14:25:54 -0700

    Added custom routes,linked to form file, link_to items, updated portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,17 +8,43 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-
-      else
-        format.html { render :new }
-
-      end
+    if @portfolio_item.save
+      redirect_to @portfolio_item
+    else
+      render :new
     end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    if @portfolio_item.update(portfolio_params)
+      redirect_to @portfolio_item
+    else
+      render :edit
+    end  
+  end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    redirect_to portfolios_path
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
   end
 
 end
