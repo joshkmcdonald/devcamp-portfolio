2cfba5918318390d62aedfdc0b9ac1e7c1507a9d
joshkmcdonald <joshkmcdonald@gmail.com>
2017-01-17 17:57:27 -0700

    One last merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170116211846) do
-
-# ActiveRecord::Schema.define(version: 20170117184627) do
+ActiveRecord::Schema.define(version: 20170117184627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,8 +70,6 @@ ActiveRecord::Schema.define(version: 20170116211846) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -93,7 +88,6 @@ ActiveRecord::Schema.define(version: 20170116211846) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
->>>>>>> c86686e2c0fda66680a23b728a59c7d2055d254a
   add_foreign_key "blogs", "topics"
   add_foreign_key "technologies", "portfolios"
 end
