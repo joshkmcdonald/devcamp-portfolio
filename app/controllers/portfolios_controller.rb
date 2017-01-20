4499daa0a3f5881c07ab3139ed7ce9ce2ae82a4c
joshkmcdonald <joshkmcdonald@gmail.com>
2017-01-19 17:26:23 -0700

    implemented view helper for auth links

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -29,7 +29,7 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    3.times { @portfolio_item.technologies.build }
+    # 3.times { @portfolio_item.technologies.build }
   end
 
   def update
