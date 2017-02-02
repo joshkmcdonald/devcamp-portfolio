8434ca6eea5ead1a3264ba58a95195d363aca979
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-02 10:45:26 -0700

    Comment out code for tech news page and route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,11 +42,11 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: 'Portfolio'
-      },
-      {
-        url: tech_news_path,
-        title: 'Tech News'
       }
+      # {
+      #   url: tech_news_path,
+      #   title: 'Tech News'
+      # }
     ]
   end
 
