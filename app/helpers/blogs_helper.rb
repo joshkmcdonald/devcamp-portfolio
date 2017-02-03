d9d3e9316dd3e8f395e229e893eb13082d3a4113
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-03 13:58:00 -0700

    implement font awesome icons for blog draft, publish, edit and delete

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,13 @@ module BlogsHelper
   def gravatar_helper(user)
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 40
   end
+
+  def font_blog_awesome(action)
+    case action
+    when 'delete' then fa_icon "trash"
+    when 'edit' then fa_icon "pencil"
+    when 'draft' then fa_icon "eye-slash"
+    when 'published' then fa_icon "eye"
+    end
+  end
 end
