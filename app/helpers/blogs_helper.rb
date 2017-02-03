232fa0e3f54f1ee1d81801455a839f0462d0d055
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-03 14:22:53 -0700

    Add redcarpet and codify to gem file. Implement markdown for blog

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -11,4 +11,24 @@ module BlogsHelper
     when 'published' then fa_icon "eye"
     end
   end
+
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code,language).div
+    end
+  end
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true,
+     }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
