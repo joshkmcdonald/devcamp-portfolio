076f85f405cd9c68fc1a7c647eef0548bcbffd89
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-06 09:18:20 -0700

    Update seed file to add topics for blogs and add validations for topics in blogs, add topic form element.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
+User.create!(
+  email: "test@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
+
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Phasellus condimentum quam lorem, sed laoreet ipsum consectetur sed. Integer at egestas tortor. Quisque sed lacinia magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus lobortis laoreet enim, a sollicitudin justo. Etiam sed velit ut est sagittis suscipit. Curabitur sollicitudin nunc risus, in aliquam magna molestie non. Mauris a aliquam diam. Maecenas ac sodales neque, sit amet placerat enim. Proin tincidunt nulla et consectetur ultricies. Pellentesque tempor venenatis feugiat. Phasellus eu nulla lobortis, porta justo ut, facilisis justo. Proin porttitor dignissim fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat, nisl ac commodo sodales, dui orci commodo nisi, a accumsan nisl ipsum at urna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    topic_id: Topic.last.id
   )
 end
 
@@ -9,22 +38,39 @@ puts "10 blog posts created"
 
 5.times do |skill|
   Skill.create!(
-    title: "rails #{skill}",
+    title: "Rails #{skill}",
     percent_utilized: 15
   )
 end
 
 puts "5 skills created"
 
-
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
-    body: "Phasellus condimentum quam lorem, sed laoreet ipsum consectetur sed. Integer at egestas tortor. Quisque sed lacinia magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus lobortis laoreet enim, a sollicitudin justo. Etiam sed velit ut est sagittis suscipit. Curabitur sollicitudin nunc risus, in aliquam magna molestie non. Mauris a aliquam diam. Maecenas ac sodales neque, sit amet placerat enim. Proin tincidunt nulla et consectetur ultricies. Pellentesque tempor venenatis feugiat. Phasellus eu nulla lobortis, porta justo ut, facilisis justo. Proin porttitor dignissim fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat, nisl ac commodo sodales, dui orci commodo nisi, a accumsan nisl ipsum at urna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
   )
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
