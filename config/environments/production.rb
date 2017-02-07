f954ece62f478028e991302ab6796257b6678dea
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-07 15:36:21 -0700

    Integrate production call for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://jkm-portfolio.herokuapp.com', 'http://jkm-portfolio.herokuapp.com']
+  config.action_cable.url = "wws://jkm-portfolio.herokuapp.com/cable"
 end
