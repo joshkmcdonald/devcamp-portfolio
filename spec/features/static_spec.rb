597c67e26f6fde60f514cd95c23ba4c2b331d05b
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-16 12:00:06 -0700

    Precompile assets

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    visit root_path
+  end
+
   describe 'homepage' do 
     it 'can be reached successfully' do 
       visit root_path
+      expect(page.status_code).to eq (200)
+    end
+  end
+
+  describe 'contact link click' do
+    it 'Page can be successfully reached' do
+      click_on("Contact")
       expect(page.status_code).to eq (200)
     end
   end
