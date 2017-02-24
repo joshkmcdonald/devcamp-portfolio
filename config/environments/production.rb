f4aedeef0b460d2de05881d916f97b7bf8d65b2e
joshkmcdonald <joshkmcdonald@gmail.com>
2017-02-24 14:29:01 -0700

    Add newrelic app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://jkm-portfolio.herokuapp.com', 'http://jkm-portfolio.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://jkm-portfolio.herokuapp.com', 'http://jkm-portfolio.herokuapp.com', 'http://www.josh-mcdonald.com', 'https://www.josh-mcdonald.com']
   config.action_cable.url = "wws://jkm-portfolio.herokuapp.com/cable"
 end
